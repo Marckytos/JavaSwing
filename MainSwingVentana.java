555a0059719c77d72286fb53d61d4327815763f2
Marckytos <114682944+Marckytos@users.noreply.github.com>
2023-05-06 13:19:50 -0600

    Add files via upload

---
+++ MainSwingVentana.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class MainSwingVentana {
+    public static void main(String[] args) {
+
+        VentanaSwing ventana = new VentanaSwing("Conversor de grados C. a F.");
+    }
+}
